b91fc83e5eb337db2df33af465e258dde135dee0
tableonthewall <seydaozdemir07@gmail.com>
2021-07-17 02:29:39 +0300

    init commit

---
+++ src/PozitifNegatifSifir.java
@@ -0,0 +1,18 @@
+package src;
+
+import java.util.Scanner;
+
+public class PozitifNegatifSifir {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Bir sayı giriniz: ");
+        int x=scanner.nextInt();
+        if(x>0){
+            System.out.println("pozitif sayı");
+        }else if(x<0){
+            System.out.println("negatif sayı");
+        }else{
+            System.out.println("sayı sıfırdır.");
+        }
+    }
+}
